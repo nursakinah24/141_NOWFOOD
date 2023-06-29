9692776cb57be0ab8c175a1a735203b3e7fae076
Nur Sakinah <nursakinah2402@gmail.com>
2023-06-29 21:08:33 +0700

    connect to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
+//import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:nowfood/view/splashscreen.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
+/* Future main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+   runApp(const MyApp()); */
+//}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -12,6 +21,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -22,9 +32,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home:  const Splash(),
     );
   }
 }
