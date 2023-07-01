949b3e5219ee7c436183b989f3e19312c51d6608
Nur Sakinah <nursakinah2402@gmail.com>
2023-07-02 04:48:44 +0700

    add values manager

---
+++ lib/manager/values_manager.dart
@@ -0,0 +1,44 @@
+class MarginManager {
+  static const double marginXS = 8.0;
+  static const double marginS = 12.0;
+  static const double marginM = 14.0;
+  static const double marginSemiM = 16.0;
+  static const double marginL = 18.0;
+  static const double marginXL = 20.0;
+}
+
+class PaddingManager {
+  static const double paddingXXS = 8.0;
+  static const double paddingXS = 10.0;
+  static const double paddingS = 12.0;
+  static const double paddingM = 14.0;
+  static const double paddingSemiM = 16.0;
+  static const double paddingL = 18.0;
+  static const double paddingXL = 20.0;
+}
+
+class SizeManager {
+  static const double sizeXS = 5.0;
+  static const double sizeS = 8.0;
+  static const double sizeM = 12.0;
+  static const double sizeSemiM = 14.0;
+  static const double sizeL = 18.0;
+  static const double sizeXL = 20.0;
+  static const double sizeXXL = 100.0;
+  static const double sizeXXXL = 240.0;
+
+  static const double splashIconSize = 150.0;
+  static const double imageSize = 230.0;
+  static const double imageSizeMed = 200.0;
+}
+
+class SpacingManager {
+  static const double spaceS = 2.0;
+  static const double spaceM = 6.0;
+  static const double spaceL = 12.0;
+}
+
+class RadiusManager {
+  static const double buttonRadius = 8.0;
+  static const double fieldRadius = 8.0;
+}
